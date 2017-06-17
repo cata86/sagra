733caabcd4c140e9a5f1269025012f7764e2078a
Alessia <a.bellini@cineca.it>
2017-06-17 19:53:25 +0200

    Modifiche css

--- www/build/app.js
+++ www/build/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'App' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('App', ['ionic', 'ngCordova', 'ngAnimate','ngLodash'])
+angular.module('App', ['ionic', 'ngCordova', 'ngAnimate','ngLodash', 'tabSlideBox'])
 
 .run(['$ionicPlatform', '$sqliteService',
   function($ionicPlatform, $sqliteService) {
